9f50a23d411108e2de8591bd308502248862f04c
Akash <patel.akash@outlook.com>
2021-01-24 15:53:24 +0530

    Contact info link

--- src/Components/ContactInfoList/index.js
+++ src/Components/ContactInfoList/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./ContactInfoList.scss";
 
 const ContactInfoList = (props) => {
-    const {icon, text, fontSize} = props;
+    const {icon, text, fontSize, url} = props;
     return(
-        <div className="contact-info-list">
+        <div className="contact-info-list" onClick={()=>window.open(url,"_blank")}>
                 <div className="contact-icon-box" style={{fontSize:fontSize}}>
                     <i className={`fa ${icon} contact-icon`} aria-hidden="true"/>
                 </div>
--- src/Section/Contact/ContactInfo/index.js
+++ src/Section/Contact/ContactInfo/index.js
@@ -17,21 +17,25 @@ const ContactInfo = () => {
                     fontSize="28px"
                     icon="fa-map-marker"
                     text="At & Po: Kosmadi, Ta: Kamrej, Surat, GJ, India"
+                    url="https://www.google.com/maps/place/Akash+Patel/@21.2203114,72.9879409,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x15a6af5121b8a0d3!8m2!3d21.2203064!4d72.9901296"
                 />
                 <ContactInfoList
                     fontSize="26px"
                     icon="fa-phone"
                     text="+91 81414 34065"
+                    url="tel:+918141434065"
                 />
                 <ContactInfoList
                     fontSize="20px"
                     icon="fa-envelope"
                     text="patel.akash@outlook.com"
+                    url="mailto:patel.akash@outlook.com"
                 />
                 <ContactInfoList
                     fontSize="22px"
                     icon="fa-linkedin"
                     text="linkedin.com/in/akash-a-patel"
+                    url="https://www.linkedin.com/in/akash-a-patel"
                 />
             </div>
         </div>
